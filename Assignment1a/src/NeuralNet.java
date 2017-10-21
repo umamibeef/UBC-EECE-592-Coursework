8c0785cf4366e065cf669faa5b05618d076c2800
Michel Kakulphimp <michelk@gmail.com>
2017-10-21 12:03:46 -0700

    Initial commit
    Adding my initial files for the project.

---
+++ Assignment1a/src/NeuralNet.java
@@ -0,0 +1,73 @@
+import java.lang.Math;
+
+public class NeuralNet implements NeuralNetInterface
+{
+    private double mArgA;
+    private double mArgB;
+
+    /**
+     * Constructor. (Cannot be declared in an interface, but your implementation will need one)
+     * @param argNumInputs The number of inputs in your input vector
+     * @param argNumHidden The number of hidden neurons in your hidden layer. Only a single hidden layer is supported
+     * @param argLearningRate The learning rate coefficient
+     * @param argMomentumTerm The momentum coefficient
+     * @param argA Integer lower bound of sigmoid used by the output neuron only.
+     * @param argB Integer upper bound of sigmoid used by the output neuron only.
+    */
+    public abstract NeuralNet(int argNumInputs, int argNumHidden, double argLearningRate, double argMomentumTerm, double argA, double argB)
+    {
+        // update out private variables
+        mArgA = argA;
+        mArgB = argB;
+    }
+
+    /**
+     * This methods implements the sigmoid function
+     * @param x The input
+     * @return f(x) 1 / (1 + exp(-x))
+     */
+    double sigmoid(double x)
+    {
+        return 1 / (1 + Math.pow(Math.E,-x));
+    }
+
+    /**
+     * This method implements a general sigmoid with asymptotes bounded by (a,b)
+     * @param x The input
+     * @return f(x) = (b - a) / (1 + exp(-x)) - a
+     */
+    double customSigmoid(double x)
+    {
+        return (mArgB - mArgA) * signmoid(x) + mArgA;
+    }
+
+    void initializeWeights()
+    {
+
+    }
+
+    void zeroWeights()
+    {
+
+    }
+
+    double outputFor(double[] x);
+    {
+
+    }
+
+    double train(double[] x, double argValue)
+    {
+
+    }
+
+    void save(File argFile)
+    {
+
+    }
+
+    void load(String argFileName) throws IOException
+    {
+
+    }
+}
