f00c723e96c12821b48541f1fa0aaba3f332a155
Michel Kakulphimp <michelk@gmail.com>
2017-11-04 19:27:54 -0700

    Adding assignment 2 files

---
+++ Assignment2a/src/LUTInterface.java
@@ -0,0 +1,37 @@
+/**
+ * Interface for the Look Up Table (LUT) Class
+ * You should 'implement' this interface
+ * @date 20 June 012
+ * @author sarbjit
+ *
+ */
+public interface LUTInterface extends CommonInterface
+{
+
+    /**
+     * Constructor. (You will need to define one in your implementation)
+     * @param argNumInputs The number of inputs in your input vector
+     * @param argVariableFloor An array specifying the lowest value of each variable in the input vector.
+     * @param argVariableCeiling An array specifying the highest value of each of the variables in the input vector.
+     * The order must match the order as referred to in argVariableFloor.
+     *
+     public LUT (
+     int argNumInputs,
+     int [] argVariableFloor,
+     int [] argVariableCeiling );
+     */
+
+    /**
+     * Initialise the look up table to all zeros.
+     */
+    public void initialiseLUT();
+
+    /**
+     * A helper method that translates a vector being used to index the look up table
+     * into an ordinal that can then be used to access the associated look up table element.
+     * @param X The state action vector used to index the LUT
+     * @return The index where this vector maps to
+     */
+    public int indexFor(double [] X);
+
+} // End of public interface LUT
